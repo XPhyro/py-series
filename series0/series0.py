5afd0d9e5dc96ad87b47bb05b5c4a1c40c121f98
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-03-29 07:57:28 +0300

    Organise imports

--- series0/series0.py
+++ series0/series0.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # Calculate 0.01+0.002+0.0003+0.00004+...
 
-from mpmath import *
 import time
 import datetime
+import mpmath as mp
 import input_helper as ih
 
 MIN_DPS = 4
 
 s = ih.get_int_rng("To how many decimal points should the sum be calculated? (min={MIND_DPS})", low=MIN_DPS)
 
-mp.dps = s
-n = mp.dps * 10
+mp.mp.dps = s
+n = mp.mp.dps * 10
 
-s = mpf(0)
+s = mp.mpf(0)
 
 # Time is obtained twice not to interfere with the actual timing of the summation.
 print(f"Initiated the summation on {datetime.datetime.now()}.\nSumming...")
@@ -21,7 +21,7 @@ print(f"Initiated the summation on {datetime.datetime.now()}.\nSumming...")
 t0 = time.time()
 
 for i in range(n):
-    s += (i + 1) * power(10, -2 - i)
+    s += (i + 1) * mp.power(10, -2 - i)
 
 t1 = time.time()
 
